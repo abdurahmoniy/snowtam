5b0c52f0fbe3f2054ea94afaf856a78c6f223868
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2025-06-24 14:15:29 +0500

    fix

--- src/app/(full-pages)/runway-condition/create/steps/SituationalAlertStep.tsx
+++ src/app/(full-pages)/runway-condition/create/steps/SituationalAlertStep.tsx
@@ -12,8 +12,26 @@ export default function SituationalAlertStep() {
     return (
         <div className="p-8 bg-white dark:bg-dark-2 rounded shadow">
             <div className="text-xl font-semibold mb-4">Situational Alert</div>
-            <div className="">Alert type</div>
-            <Select>
+            <div className="mb-2">Alert type</div>
+            <Select size="large" placeholder="Choose alert type">
+                {AlertTypesData.data?.data.map((item, i) => (
+                    <Select.Option key={i}>
+                        {item}
+                    </Select.Option>
+                ))}
+            </Select>
+
+            <div className="mb-2">Alert type</div>
+            <Select size="large" placeholder="Choose alert type">
+                {AlertTypesData.data?.data.map((item, i) => (
+                    <Select.Option key={i}>
+                        {item}
+                    </Select.Option>
+                ))}
+            </Select>
+
+            <div className="mb-2">Alert type</div>
+            <Select size="large" placeholder="Choose alert type">
                 {AlertTypesData.data?.data.map((item, i) => (
                     <Select.Option key={i}>
                         {item}
